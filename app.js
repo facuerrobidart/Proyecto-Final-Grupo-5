53ae924761b5644e289c53c626b6c6775ef73e91
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-07-01 10:30:18 -0300

    avanzando con formulario de registro

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ app.use(express.static(path.resolve(__dirname,"./views")));
 app.listen(3000,()=>{console.log("Corriendo en puerto 3000");});
 
 app.get("/",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/home.html"));
+    res.sendFile(path.resolve(__dirname,"./views/index.html"));
 });
 
 
-
+app.get("/registro",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/registro.html"));
+});
 
