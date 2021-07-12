5cb486ab4e74a8e26aa9246b463388d6ed1aed6d
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-07-12 09:38:40 -0300

    me faltaba el process port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 app.use(express.static(path.resolve(__dirname,"./public")));
 app.use(express.static(path.resolve(__dirname,"./views")));
-app.listen(3000,()=>{console.log("Corriendo en puerto 3000");});
+app.listen(process.env.PORT || 3000,()=>{console.log("Corriendo en puerto 3000");});
 
 app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/index.html"));
