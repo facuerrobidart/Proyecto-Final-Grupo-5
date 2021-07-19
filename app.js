6222237625ed4e4d9abb3ab7eaf94255cc13570d
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-07-19 19:58:28 -0300

    method override agregado

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const path = require("path");
 const routerIndex = require("./src/routes/routerindex");
 const routerProducto = require("./src/routes/routerProducto");
 const routerUser = require("./src/routes/routeruser");
-
+const methodOverride = require("method-override");
 app.set("view engine","ejs");
 app.use(express.static(path.resolve(__dirname,"./public")));
 app.use(express.static(path.resolve(__dirname,"./views")));
 app.use("/",routerIndex);
 app.use("/producto",routerProducto);
 app.use("/user",routerUser);
-
+app.use(methodOverride("_method"));
 app.listen(process.env.PORT || 3000,()=>{console.log("Corriendo en puerto 3000");});
 
 
