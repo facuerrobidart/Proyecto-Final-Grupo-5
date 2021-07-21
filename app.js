37ee1fa26ff40f19a8e0d1e93d9c47a0096e420d
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-07-21 19:37:21 -0300

    nodemon

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const routerIndex = require("./src/routes/routerindex");
 const routerProducto = require("./src/routes/routerProducto");
 const routerUser = require("./src/routes/routeruser");
 const methodOverride = require("method-override");
+
 app.set("view engine","ejs");
 app.use(express.static(path.resolve(__dirname,"./public")));
 app.use(express.static(path.resolve(__dirname,"./views")));
@@ -12,6 +13,7 @@ app.use("/",routerIndex);
 app.use("/producto",routerProducto);
 app.use("/user",routerUser);
 app.use(methodOverride("_method"));
+
 app.listen(process.env.PORT || 3000,()=>{console.log("Corriendo en puerto 3000");});
 
 
