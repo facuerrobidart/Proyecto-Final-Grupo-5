13187abcb6d34948b9c2681644bffbad3b13d5f9
Emiliobau <emiliobau@hotmail.com>
2021-08-01 12:14:59 -0300

    creacion JSON productos, ruta y controler para creacion de productos

--- src/controllers/controllerProducto.js
+++ src/controllers/controllerProducto.js
@@ -1,3 +1,12 @@
+
+const fs = require("fs");
+const path = require("path");
+
+const pathProductos = path.join(__dirname,"../../src/data/productosDataBase.json");
+const productos = JSON.parse(fs.readFileSync(pathProductos,"utf-8"));
+
+
+
 const controller = {
     producto: (req,res)=>{
         res.render("./products/producto");
@@ -8,6 +17,40 @@ const controller = {
     crear: (req, res) => {
         res.render("./products/crearProducto");
     },
+    crearProducto:(req, res) => {
+
+        nombreFoto = req.file.filename
+
+        idProducto = 0
+        for (i=0; i<productos.length; i++){
+            if(idProducto < productos[i].id){
+                 idProducto = idProducto +1
+            }
+        }
+        idProducto = idProducto + 1
+
+        productoNuevo = {
+            id: idProducto,
+            categoriasProducto: req.body.categoriasProducto,
+            fotoProducto: nombreFoto,
+            nombreArtista: req.body.nombreArtista,
+            caracteristicasProducto: req.body.caracteristicasProducto,
+            condicionProducto: req.body.condicionProducto,
+            precio: req.body.precio
+        }
+
+        productos.push(productoNuevo)
+
+        fs.writeFileSync(pathProductos, JSON.stringify(productos,null, " "))
+
+        res.redirect("/")
+
+
+
+
+
+    },
+
     editar: (req, res) =>{
         res.render("./products/editarProducto");
     }
--- src/routes/routerProducto.js
+++ src/routes/routerProducto.js
@@ -1,12 +1,30 @@
 const express=require("express");
 const router = express.Router();
 const controller = require("./../controllers/controllerProducto");
+const multer = require("multer")
+const path = require("path")
+
+// SETTING Multer
+
+
+const storage = multer.diskStorage({
+  destination:( req, file, cb)=>{
+    cb(null, path.join(__dirname, "../../public/images"))
+  },
+  filename:(req, file, cb)=>{
+    let nombreFoto = file.fieldname+"-"+Date.now()+ path.extname(file.originalname)
+    cb(null, nombreFoto)
+  }
+})
+
+const upload = multer( {storage:storage})
 
 
 
 router.get("/",controller.producto);
 router.get("/carrito",controller.carrito);
 router.get("/crear", controller.crear);
+router.post("/crear", upload.single("fotoProducto"), controller.crearProducto)
 router.get("/editar", controller.editar)
 
 
