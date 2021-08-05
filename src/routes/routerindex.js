8161e70fec57f72a65a20b83109b82e02f83335d
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-08-05 17:15:08 -0300

    bug fix heroku

--- src/routes/routerindex.js
+++ src/routes/routerindex.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/controllerIndex");
+const controller = require("./../controllers/controllerIndex");
 
 
 router.get("/", controller.index);
--- src/routes/routeruser.js
+++ src/routes/routeruser.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/controllerUser");
+const controller = require("./../controllers/controllerUser");
 
 
 router.get("/login", controller.login);
