0a720ba972fc4b62c63246ceff1692f8b5157980
Emiliobau <emiliobau@hotmail.com>
2021-08-10 19:45:40 -0300

    mas cambios

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
 app.use(cookieParser());
-app.use(session({secret: "este es un secreto"}));
+app.use(session(
+  {secret: "este es un secreto",
+   resave: false,
+   saveUninitialized: false
+}));
 
 //CARPETAS PUBLICAS
 app.use(express.static(path.resolve(__dirname,"./public")));
