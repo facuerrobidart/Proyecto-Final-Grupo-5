afcd14526595fd2c863a19052b0aa91927e9db1d
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-09-10 16:24:18 -0300

    modelado tipos de usuario

---
+++ src/database/models/tipoUsuario.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize,DataTypes)=>{
+    const alias = "tipos_usuario";
+
+    const cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        nombre_tipos_usuario:{
+            type: DataTypes.STRING(45),
+            allowNull: false
+        }
+    }
+
+    const config = {timestamps:false};
+
+    const tipoUsuario = sequelize.define(alias,cols,config);
+
+    tipoUsuario.associate = (models) => {
+        tipoUsuario.hasMany(models.usuario,{
+            as: "usuarios",
+            foreignKey: "tipos_usuario_id"
+        })
+    }
+}
