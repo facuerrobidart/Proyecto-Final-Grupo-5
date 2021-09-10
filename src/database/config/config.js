da0a7da9cf1c16473d8f6498ba1696b331b0080d
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-09-10 12:11:19 -0300

    config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,17 +7,17 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "241888",
+    "password": "DigitalHouse123",
+    "database": "facuerrobidart_proyectodh",
+    "host": "mysql-facuerrobidart.alwaysdata.net",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "241888",
+    "password": "DigitalHouse123",
+    "database": "facuerrobidart_proyectodh",
+    "host": "mysql-facuerrobidart.alwaysdata.net",
     "dialect": "mysql"
   }
 }
