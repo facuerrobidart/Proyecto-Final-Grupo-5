3646ada1ed89680b99f007bb67d88e162af79071
Pablo Zajac <pzajac@hda.com.ar>
2021-09-23 23:04:18 -0300

    requerido para poder pullear

--- src/controllers/controllerProducto.js
+++ src/controllers/controllerProducto.js
@@ -16,106 +16,103 @@ const controller = {
     producto: (req, res) => {
         let id = req.params.id;
         db.productos.findByPk(id)
-        .then((product)=>{
-            let producto = product;
-            if(producto != undefined){
-                res.render("./products/producto", { producto: producto })
-            }
-            else
-            {res.send("El articulo no existe!!!")}
-        })
+            .then((product) => {
+                let producto = product;
+                if (producto != undefined) {
+                    res.render("./products/producto", { producto: producto })
+                }
+                else { res.send("El articulo no existe!!!") }
+            })
     },
     carrito: (req, res) => {
         res.render("./products/carrito-compra");
     },
     crear: (req, res) => {
-        let usuarioVendedor= req.session.usuarioLogueado;
+        let usuarioVendedor = req.session.usuarioLogueado;
         idVendedor = usuarioVendedor[0].id;
 
         let categoriasProducto = db.categorias_producto.findAll();
         let condicionProducto = db.condiciones_producto.findAll();
         Promise.all([categoriasProducto, condicionProducto])
-            .then(([categorias, condiciones])=>{
+            .then(([categorias, condiciones]) => {
 
-                res.render("./products/crearProducto", {categorias: categorias, condiciones: condiciones, idVendedor: idVendedor
+                res.render("./products/crearProducto", {
+                    categorias: categorias, condiciones: condiciones, idVendedor: idVendedor
                 })
             })
 
     },
-<<<<<<< HEAD
-=======
     crearProducto: (req, res) => {
-         let nombreImagen = req.file.filename
-         db.productos.create({
-             titulo: req.body.titulo,
-             descripcion: req.body.caracteristicasProducto,
-             precio: req.body.precio,
-             nombre_artista: req.body.nombreArtista,
-             stock: req.body.stock,
-             categorias_producto_id: req.body.categoriasProducto,
-             condiciones_producto_id: req.body.condicionProducto,
-             usuarios_vendedor_id: req.body.idVendedor,
-             nombre_imagen: nombreImagen
-         })
+        let nombreImagen = req.file.filename
+        db.productos.create({
+            titulo: req.body.titulo,
+            descripcion: req.body.caracteristicasProducto,
+            precio: req.body.precio,
+            nombre_artista: req.body.nombreArtista,
+            stock: req.body.stock,
+            categorias_producto_id: req.body.categoriasProducto,
+            condiciones_producto_id: req.body.condicionProducto,
+            usuarios_vendedor_id: req.body.idVendedor,
+            nombre_imagen: nombreImagen
+        })
 
         res.redirect("/producto/all")
     },
->>>>>>> 2f072fd68fa92cfc95402740d994bce0f9f72425
 
     all: (req, res) => {
         db.productos.findAll()
-        .then((productos)=>{
-            let products = productos
-            res.render("./products/listaProductos", { products: products });
-        })
+            .then((productos) => {
+                let products = productos
+                res.render("./products/listaProductos", { products: products });
+            })
     },
 
     editar: (req, res) => {
 
         let idProducto = req.params.id;
         db.productos.findByPk(idProducto)
-        .then((producto=>{
-            let ProductoaEditar = producto
-            res.render("./products/editarProducto", { ProductoaEditar: ProductoaEditar });
-        }))
+            .then((producto => {
+                let ProductoaEditar = producto
+                res.render("./products/editarProducto", { ProductoaEditar: ProductoaEditar });
+            }))
     },
 
     actualizar: (req, res) => {
 
         let id = req.params.id
         let nombreImagen
-        if(req.file){
+        if (req.file) {
             nombreImagen = req.file.filename
         }
 
-            db.productos.update({
-                titulo: req.body.titulo,
-                descripcion: req.body.descripcion,
-                precio: req.body.precio,
-                nombre_artista: req.body.nombre_artista,
-                nombre_imagen: nombreImagen,
-            },
-        {
-            where: {id:id}
-        })
+        db.productos.update({
+            titulo: req.body.titulo,
+            descripcion: req.body.descripcion,
+            precio: req.body.precio,
+            nombre_artista: req.body.nombre_artista,
+            nombre_imagen: nombreImagen,
+        },
+            {
+                where: { id: id }
+            })
         res.redirect("/producto/all")
 
     },
-    delete:(req, res)=>{
+    delete: (req, res) => {
         id = req.params.id
         console.log(id)
         db.productos.destroy({
-            where: { id : id}
+            where: { id: id }
         })
-        .then((resultado)=>{
-            res.redirect("/producto/all")
-        })
+            .then((resultado) => {
+                res.redirect("/producto/all")
+            })
             .catch((error) => {
                 res.send(error)
             })
     }
 
-    };
+};
 
 
 module.exports = controller;
