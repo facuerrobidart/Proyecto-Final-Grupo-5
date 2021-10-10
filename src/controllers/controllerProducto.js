e90f173b07586a31661a8be19bb99b30c17a4406
Emiliobau <emiliobau@hotmail.com>
2021-10-10 09:46:10 -0300

    modificacion ControllerProducto

--- src/controllers/controllerProducto.js
+++ src/controllers/controllerProducto.js
@@ -3,11 +3,7 @@ const { promiseImpl } = require("ejs");
 const fs = require("fs");
 const { dirname } = require("path");
 const path = require("path");
-<<<<<<< HEAD
-var session = require('express-session');
-=======
 const {validationResult} = require("express-validator");
->>>>>>> af1784b3cb53d9e16ac5f8e6308a474f389dbda5
 const pathProductos = path.join(__dirname, "../../src/data/productosDataBase.json");
 const productos = JSON.parse(fs.readFileSync(pathProductos, "utf-8"));
 
@@ -53,20 +49,6 @@ const controller = {
             nombreImagen = req.file.filename
         }
 
-         db.productos.create({
-             titulo: req.body.titulo,
-             descripcion: req.body.caracteristicasProducto,
-             precio: req.body.precio,
-             nombre_artista: req.body.nombreArtista,
-             stock: req.body.stock,
-             categorias_producto_id: req.body.categoriasProducto,
-             condiciones_producto_id: req.body.condicionProducto,
-             usuarios_vendedor_id: req.body.idVendedor,
-             nombre_imagen: nombreImagen
-         })
-
-        res.redirect("/producto/all")
-        let nombreImagen = req.file.filename
         let errors = validationResult(req);
         if ((req.file.mimetype=="image/jpeg" || req.file.mimetype=="image/png") && errors.isEmpty){ //valido mimetype y me traigo las validaciones del middleware
             db.productos.create({
