d8fe140fce462fc72dd9362a87fcd9c5275b10c9
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-10-13 20:39:57 -0300

    fix middleware

--- public/js/agregaCarrito.js
+++ public/js/agregaCarrito.js
@@ -1,3 +1,11 @@
-function agregaCarrito(producto){
-    console.log(producto);
+function agregaCarrito(titulo,precio,nombre_imagen){
+    console.log(titulo,precio,nombre_imagen);
+    let agregar = {
+        titulo: titulo,
+        precio: precio,
+        nombre_imagen: nombre_imagen
+    }
+
+    let productos = localStorage.getItem("productos");
+    console.log(productos);
 }
--- src/middleware/edicionAutorizada.js
+++ src/middleware/edicionAutorizada.js
@@ -4,8 +4,9 @@ let db = require("../database/models");
 function edicionAutorizada(req, res, next) {
     db.productos.findByPk(req.params.id)
         .then((product) => {
-            if (req.session.usuarioLogueado.id != product.usuarios_vendedor_id)
+            if (req.session.usuarioLogueado.id != product.usuarios_vendedor_id){
                 return res.redirect("/");
+            }
             else{
                 next();
             }
