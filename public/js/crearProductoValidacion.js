bb74885076cd8b086142972aeeaae6366753091c
Emiliobau <emiliobau@hotmail.com>
2021-10-13 20:21:23 -0300

    actualizacion Validacion Form Producto

---
+++ public/js/crearProductoValidacion.js
@@ -0,0 +1,16 @@
+
+window.onload=function(){
+
+let bienvenida = document.querySelector("#bienvenida")
+let contanos = document.querySelector("#contanos")
+
+bienvenida.addEventListener("mouseover", function(){
+  bienvenida.style.color = "blue"
+})
+contanos.addEventListener("mouseover", function(){
+  contanos.style.color = "blue"
+})
+
+
+
+}
