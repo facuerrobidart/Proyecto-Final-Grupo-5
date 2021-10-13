b6a8cd1f04506fbb2f5bb50203bddb5bdb6dc2af
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-10-13 20:53:53 -0300

    fix middleware

--- src/middleware/edicionAutorizada.js
+++ src/middleware/edicionAutorizada.js
@@ -4,11 +4,11 @@ let db = require("../database/models");
 function edicionAutorizada(req, res, next) {
     db.productos.findByPk(req.params.id)
         .then((product) => {
-            if (req.session.usuarioLogueado.id != product.usuarios_vendedor_id){
-                return res.redirect("/");
+            if (req.session.usuarioLogueado[0].id === product.usuarios_vendedor_id){
+                next();
             }
             else{
-                next();
+                return res.redirect("/");
             }
         });
 }
