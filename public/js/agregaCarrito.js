26a360184f7551638f00b930c7b71bd756157a1d
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-10-13 21:05:25 -0300

    LOGICA CARRITO

--- public/js/agregaCarrito.js
+++ public/js/agregaCarrito.js
@@ -5,7 +5,16 @@ function agregaCarrito(titulo,precio,nombre_imagen){
         precio: precio,
         nombre_imagen: nombre_imagen
     }
+    var productos = localStorage.getItem("productos");
 
-    let productos = localStorage.getItem("productos");
+    if (productos=="[object Object]"){ //si el array esta vacio
+        productos=[]; //inicializo el array vacio
+    }else{
+        productos = JSON.parse(productos); //convierto el json en variable para operar
+    }
+
+    productos.push(agregar);
+    localStorage.setItem("productos",JSON.stringify(productos));
+    
     console.log(productos);
 }
