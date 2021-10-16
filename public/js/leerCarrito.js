7ca7ef5ca009c435e6ea8a553a42a03a13b626f5
Pablo Zajac <pzajac@hda.com.ar>
2021-10-16 08:13:05 -0300

    carrito compra totalizador

--- public/js/leerCarrito.js
+++ public/js/leerCarrito.js
@@ -5,21 +5,28 @@ var titulo = document.querySelector('.descripcion');
 var precio = document.querySelector('.precio');
 let cantidad = document.querySelector('.cantidad');
 let subTotal = document.querySelector('.total');
-let total = document.querySelector('.total');
+let total = document.querySelector('.totalizador');
 
 //let productos = localStorage.getItem("productos");
 productos = JSON.parse(localStorage.getItem("productos"));
 
 //console.log(lista);
 
+
+let totalizador = 0;
+
 for (let i = 0; i < productos.length; i++) {
     titulo.innerHTML += '<li>' + productos[i].titulo + '</li>';
     precio.innerHTML += '<li>' + "$ " + productos[i].precio + '</li>';
     cantidad.innerHTML += '<li>' + 1 + '</li>';
     subTotal.innerHTML += '<li>' + "$ " + 1 * productos[i].precio + '</li>';
+    totalizador = totalizador + (productos[i].precio * 1);
+    total.innerHTML = "$ " + totalizador;
 }
 
 
-
+function vaciarCarrito() {
+    localStorage.clear();
+}
 
 
