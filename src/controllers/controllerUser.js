b4ddc7489495578ed154057ded7f022c999bbd4b
Pablo Zajac <pzajac@hda.com.ar>
2021-10-20 01:35:18 -0300

    API usuarios

--- src/controllers/controllerUser.js
+++ src/controllers/controllerUser.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const { dirname } = require("path");
 const path = require("path");
-const {body,validationResult} = require("express-validator");
+const { body, validationResult } = require("express-validator");
 const bcrypt = require('bcryptjs');
 
 const pathUsuarios = path.join(__dirname, "../../src/data/userDataBase.json");
@@ -13,90 +13,121 @@ const controller = {
     login: (req, res) => {
         res.render("./users/login");
     },
-    procesoLogin:(req, res)=>{
+    procesoLogin: (req, res) => {
         let error = validationResult(req);
         emailUsuario = req.body.email
         db.usuarios.findAll({
-            where: { email : emailUsuario}
+            where: { email: emailUsuario }
         })
-            .then((usuario)=>{
+            .then((usuario) => {
                 let usuarioLogueado = usuario
                 let contrasenaUsuario
-                if(usuarioLogueado){
-                    for( let i=0; i<usuarioLogueado.length; i++){
-                        contrasenaUsuario = usuarioLogueado[i].contrasena }
-                let contrasenaCorrecta = bcrypt.compareSync(req.body.password, contrasenaUsuario);
-                console.log(error.array());
-                if(contrasenaCorrecta){
-                    req.session.usuarioLogueado = usuarioLogueado
-
-                    if(req.body.recordarUsuario){
-                        res.cookie("email", req.body.email, {maxAge: (1000*60)*60})
+                if (usuarioLogueado) {
+                    for (let i = 0; i < usuarioLogueado.length; i++) {
+                        contrasenaUsuario = usuarioLogueado[i].contrasena
                     }
-                     res.redirect("/user/info")
+                    let contrasenaCorrecta = bcrypt.compareSync(req.body.password, contrasenaUsuario);
+                    console.log(error.array());
+                    if (contrasenaCorrecta) {
+                        req.session.usuarioLogueado = usuarioLogueado
+
+                        if (req.body.recordarUsuario) {
+                            res.cookie("email", req.body.email, { maxAge: (1000 * 60) * 60 })
+                        }
+                        res.redirect("/user/info")
+                    }
+                    else if (error.isEmpty()) {
+                        res.render("./users/login", { mensaje: ("las credenciales son invalidas") });
+                    } else if (error.array()[0].param == "password") {
+                        res.render("./users/login", { mensaje: ("La contraseña debe tener al menos 6 caracteres") });
+                    } else {
+                        res.render("./users/login", { mensaje: ("Ingrese un email válido") });
+                    }
+
                 }
-                else if (error.isEmpty()){
-                    res.render("./users/login", {mensaje: ("las credenciales son invalidas") });
-                }else if(error.array()[0].param=="password"){
-                    res.render("./users/login",{mensaje: ("La contraseña debe tener al menos 6 caracteres")});
-                }else{
-                    res.render("./users/login",{mensaje: ("Ingrese un email válido")});
-                }
-                
-            }
             })
-            .catch((error)=>{
-                res.render("./users/login", { mensaje: ("email incorrecto intentelo nuevamente")
-            })})
+            .catch((error) => {
+                res.render("./users/login", {
+                    mensaje: ("email incorrecto intentelo nuevamente")
+                })
+            })
 
     },
-    logout:(req, res)=>{
+    logout: (req, res) => {
         res.clearCookie("email")
         req.session.destroy()
         res.redirect("/")
     },
     registro: (req, res) => {
-         db.tipos_usuario.findAll()
-            .then((tiposUsuarios)=>{
-                let tipos_usuario=tiposUsuarios
-                res.render("./users/registro", { tipos_usuario:tipos_usuario});
+        db.tipos_usuario.findAll()
+            .then((tiposUsuarios) => {
+                let tipos_usuario = tiposUsuarios
+                res.render("./users/registro", { tipos_usuario: tipos_usuario });
             })
 
     },
     registroUsuario: (req, res) => {
         let errors = validationResult(req);
         let tipos_usuario;
-        
+
         db.tipos_usuario.findAll()
-            .then((tiposUsuarios)=>{
-                tipos_usuario=tiposUsuarios;
+            .then((tiposUsuarios) => {
+                tipos_usuario = tiposUsuarios;
             });
 
-        if (errors.isEmpty()){ //si no hay errores procedo
+        if (errors.isEmpty()) { //si no hay errores procedo
             db.usuarios.create({
                 nombre: req.body.nombre,
                 apellido: req.body.apellido,
-                email:req.body.email,
+                email: req.body.email,
                 contrasena: bcrypt.hashSync(req.body.contrasena, 5),
-                direccion:req.body.direccion,
+                direccion: req.body.direccion,
                 ciudad: req.body.ciudad,
                 provincia: req.body.provincia,
                 codigo_postal: req.body.codigoPostal,
                 tipos_usuario_id: req.body.tipoUsuario,
             })
             res.redirect("/");
-        }else{ //si no, devuelvo errores
-            res.render("./users/registro",{tipos_usuario:tipos_usuario,errores:errors});
+        } else { //si no, devuelvo errores
+            res.render("./users/registro", { tipos_usuario: tipos_usuario, errores: errors });
         }
     },
 
-    info: (req,res)=>{
-        res.render("./users/info", { usuarioLogueado : req.session.usuarioLogueado});
+    info: (req, res) => {
+        res.render("./users/info", { usuarioLogueado: req.session.usuarioLogueado });
+    },
+
+    usuariosAPI: (req, res) => {
+        db.usuarios.findAll()
+            .then((usuario) => {
+                return res.status(200).json({
+                    //status: 200,
+                    count: usuario.length,
+                    users: usuario
+                });
+            })
+    },
+
+    usuarioUnicoAPI: (req, res) => {
+        let user = req.params.id;
+        db.usuarios.findAll({
+            where: { id: user },
+            attributes: ['id', 'nombre', 'apellido', 'email', 'direccion', 'ciudad', 'provincia', 'codigo_postal', 'tipos_usuario_id']
+        }
+        )
+            .then((usuario) => {
+                //console.log(usuario);
+                if (usuario.length != 0) {
+                    return res.status(200).json({
+                        //status: 200,
+                        users: usuario
+                    });
+                }
+                else { res.send("El usuario no existe!!!") }
+            })
     }
 
-
 }
-
 
 
 
--- src/routes/routeruser.js
+++ src/routes/routeruser.js
@@ -5,26 +5,26 @@ const multer = require("multer");
 const path = require("path");
 const sinUsuarioMiddleware = require("../../src/middleware/sinUsuarioMiddleware")
 const conUsuarioMiddleware = require("../../src/middleware/conUsuarioMiddleware")
-const {body,check,validationResult} = require("express-validator");
+const { body, check, validationResult } = require("express-validator");
 
 const validaLogin = [check("email").isEmail(),
-                    check("password").isLength({min:6})];
+check("password").isLength({ min: 6 })];
 const validaRegistro = [check("email")
-                                        .isEmail(),
-                        check("contraseña")
-                                        .isLength({min:8}),
-                        check("nombre")
-                                        .notEmpty(),
-                        check("apellido")
-                                        .notEmpty(),
-                        check("codigo_postal")
-                                        .notEmpty().withMessage("El CP no puede estar vacío"),
-                        check("direccion")
-                                        .isString()
-                                        .notEmpty().withMessage("La dirección no puede estar vacia"),
-                        check("ciudad")
-                                        .isString()
-                                        .notEmpty().withMessage("La ciudad no puede estar vacia")];
+    .isEmail(),
+check("contraseña")
+    .isLength({ min: 8 }),
+check("nombre")
+    .notEmpty(),
+check("apellido")
+    .notEmpty(),
+check("codigo_postal")
+    .notEmpty().withMessage("El CP no puede estar vacío"),
+check("direccion")
+    .isString()
+    .notEmpty().withMessage("La dirección no puede estar vacia"),
+check("ciudad")
+    .isString()
+    .notEmpty().withMessage("La ciudad no puede estar vacia")];
 
 // SETTING Multer
 const storage = multer.diskStorage({
@@ -39,12 +39,14 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-router.get("/login",conUsuarioMiddleware, controller.login);
-router.post("/login",validaLogin,controller.procesoLogin);
+router.get("/login", conUsuarioMiddleware, controller.login);
+router.post("/login", validaLogin, controller.procesoLogin);
 router.get("/register", conUsuarioMiddleware, controller.registro);
-router.post("/register",validaRegistro,upload.single("imageUser"), controller.registroUsuario);
-router.get("/info",sinUsuarioMiddleware, controller.info);
+router.post("/register", validaRegistro, upload.single("imageUser"), controller.registroUsuario);
+router.get("/info", sinUsuarioMiddleware, controller.info);
 router.get("/logout", controller.logout);
-
+//API's
+router.get("/usuariosAPI", controller.usuariosAPI);
+router.get("/usuarioAPI/:id", controller.usuarioUnicoAPI);
 
 module.exports = router;
