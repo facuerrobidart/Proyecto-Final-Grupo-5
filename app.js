8c9cff7c242891fcdc1a9bbcf3c5ed335620eeb3
Pablo Zajac <pzajac@hda.com.ar>
2021-10-27 19:55:21 -0300

    cambio de port

--- app.js
+++ app.js
@@ -36,6 +36,6 @@ app.use("/user", routerUser);
 
 
 
-app.listen(process.env.PORT || 3000, () => { console.log("Corriendo en puerto 3000"); });
+app.listen(process.env.PORT || 3001, () => { console.log("Corriendo en puerto 3001"); });
 
 
