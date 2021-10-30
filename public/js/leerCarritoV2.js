83063aa48e5a502efd0f1e858835f3a53cb14196
Pablo Zajac <pzajac@hda.com.ar>
2021-10-30 01:34:00 -0300

    mejoras en home y logos

--- public/js/leerCarritoV2.js
+++ public/js/leerCarritoV2.js
@@ -11,7 +11,7 @@ for (let prod of productos) {
     console.log(prod);
     subtotal += parseFloat(prod.precio);
     let param = "'" + prod.titulo + "'"
-    contieneCarrito.innerHTML += '<div class="row"><div class="row main align-items-center"><div class="col-2"><img class="img-fluid" src=/images/' + prod.nombre_imagen + '></div><div class="col"><div class="row text-muted">Shirt</div><div class="row">' + prod.titulo + '</div></div><div class="col"> <a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a> </div><div class="col">&dollar;' + parseFloat(prod.precio) + '<span class="close" onclick="eliminarCarrito(' + param + ')" >&#10005;</span></div></div></div>'
+    contieneCarrito.innerHTML += '<div class="row"><div class="row main align-items-center"><div class="col-2"><img class="img-fluid" src=/images/' + prod.nombre_imagen + '></div><div class="col"><div class="row text-muted">Item</div><div class="row">' + prod.titulo + '</div></div><div class="col"> <a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a> </div><div class="col">&dollar;' + parseFloat(prod.precio) + '<span class="close" onclick="eliminarCarrito(' + param + ')" >&#10005;</span></div></div></div>'
 }
 divTotales.innerHTML = '<div class="col" style="padding-left:0;">Items: ' + productos.length + '</div><div class="col text-right">&dollar;' + parseFloat(subtotal) + '</div>';
 total.innerHTML = '&dollar; ' + parseFloat(subtotal);
@@ -22,7 +22,7 @@ function refresh() {
     for (let prod of productos) {
         console.log(prod);
 
-        contieneCarrito.innerHTML += '<div class="row"><div class="row main align-items-center"><div class="col-2"><img class="img-fluid" src=/images/' + prod.nombre_imagen + '></div><div class="col"><div class="row text-muted">Shirt</div><div class="row">' + prod.titulo + '</div></div><div class="col"> <a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a> </div><div class="col">&dollar;' + parseFloat(prod.precio) + '<span class="close" onclick=eliminarCarrito(' + prod.titulo + ') >&#10005;</span></div></div></div>'
+        contieneCarrito.innerHTML += '<div class="row"><div class="row main align-items-center"><div class="col-2"><img class="img-fluid" src=/images/' + prod.nombre_imagen + '></div><div class="col"><div class="row text-muted">Item</div><div class="row">' + prod.titulo + '</div></div><div class="col"> <a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a> </div><div class="col">&dollar;' + parseFloat(prod.precio) + '<span class="close" onclick=eliminarCarrito(' + prod.titulo + ') >&#10005;</span></div></div></div>'
     }
 
     divTotales.innerHTML = '<div class="col" style="padding-left:0;">Items: ' + productos.length + '</div><div class="col text-right">&dollar;' + parseFloat(subtotal) + '</div>';
